15161115bcc0e3d30283005600fe573339349fd6
Daniel1209 <danielgarciamendoza.204@gmail.com>
2022-10-05 02:29:24 -0500

    TicTacToe

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import Home from "./views/Home";
 import YoGiHo from "./views/YoGiHo";
 import Pokemons from "./views/Pokemons";
+import TicTacToe from "./views/TicTacToe";
 
 const App = () => {
-  return <Pokemons />;
+  return <TicTacToe />;
 };
 
 export default App;
